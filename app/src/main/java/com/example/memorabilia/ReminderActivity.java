2a55731327b1e0d293be5253f059f2987eda445d
Sanchit <sanaro1999@gmail.com>
2020-05-28 02:34:44 +0530

    Initial app

---
+++ app/src/main/java/com/example/memorabilia/ReminderActivity.java
@@ -0,0 +1,14 @@
+package com.example.memorabilia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ReminderActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reminder);
+    }
+}
