52c5bf73766f650a6766fc04206a5ecf630c3124
Sanchit <sanaro1999@gmail.com>
2020-06-25 00:40:37 +0530

    UI changes

--- app/src/main/java/com/example/memorabilia/MainActivity.java
+++ app/src/main/java/com/example/memorabilia/MainActivity.java
@@ -2,13 +2,98 @@ package com.example.memorabilia;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editTextTime, editTextDate;
+    Date date;
+    public int year, month, day, hr, min;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        editTextDate = findViewById(R.id.editTextDate);
+        editTextTime = findViewById(R.id.editTextTime);
+
+        editTextTime.setLongClickable(false);
+        editTextTime.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Calendar mcurrentTime = Calendar.getInstance();
+                final int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                final int minute = mcurrentTime.get(Calendar.MINUTE);
+                TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        min = selectedMinute;
+                        hr = selectedHour;
+
+                        if(selectedHour > 12)
+                        {
+                            editTextTime.setText(selectedHour-12 + ":" + selectedMinute + " PM");
+                        }
+                        else if(selectedHour == 12)
+                        {
+                            editTextTime.setText(selectedHour + ":" + selectedMinute + " PM");
+                        }
+                        else if(selectedHour == 0)
+                        {
+                            editTextTime.setText(selectedHour+12 + ":" + selectedMinute + " AM");
+                        }
+                        else
+                        {
+                            editTextTime.setText(selectedHour + ":" + selectedMinute + " AM");
+                        }
+
+                    }
+                }, hour, minute, false);
+                mTimePicker.setTitle("Select Time");
+                mTimePicker.show();
+
+            }
+        });
+
+        editTextDate.setLongClickable(false);
+        editTextDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Calendar mcurrentDate = Calendar.getInstance();
+                DatePickerDialog mDatePicker;
+                mDatePicker = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+
+                    @Override
+                    public void onDateSet(DatePicker DatePicker, int selectedYear, int selectedMonth, int selectedDay) {
+                        year = selectedYear;
+                        month = selectedMonth;
+                        day = selectedDay;
+                        Calendar newDate = Calendar.getInstance();
+                        newDate.set(selectedYear, selectedMonth, selectedDay);
+                        SimpleDateFormat dateFormat = new SimpleDateFormat("EEE, dd MMM yyyy");
+                        String dateString = dateFormat.format(newDate.getTime());
+                        editTextDate.setText(dateString);
+                    }
+                }, mcurrentDate.get(Calendar.YEAR), mcurrentDate.get(Calendar.MONTH), mcurrentDate.get(Calendar.DAY_OF_MONTH));
+
+                mDatePicker.setTitle("Select Date");
+                mDatePicker.show();
+
+            }
+        });
     }
 }
